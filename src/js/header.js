4225401a1240d9dfe587b8c91e2ff6287cba7c6f
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-11-13 10:55:10 -0800

    add header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,13 @@
 //header
+const menuOpen = document.getElementById("js-header-sp-close");
+const menuFull = document.getElementById("js-header-sp-opened");
+const btnClose = document.getElementById("js-header-sp-btn-close");
+const btnOpen = document.getElementById("js-header-sp-btn-open");
+
+function menuShow() {
+  console.log("open");
+  console.log(menuFull.style.display);
+}
+function menuHide() {}
+btnOpen.addEventListener("click", menuShow);
+btnClose.addEventListener("click", menuHide);
