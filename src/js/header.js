77ac3f52bedb95077367014b64cf4a517aa92971
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-11-13 20:39:52 -0800

    add header-bg

--- src/js/header.js
+++ src/js/header.js
@@ -2,32 +2,32 @@
 
 //header size
 const main = document.querySelector("main");
-const spHeader = document.getElementById("js-nav-sp");
-const pcHeader = document.getElementById("js-nav-pc");
+const navSP = document.getElementById("js-nav-sp");
+const navPC = document.getElementById("js-nav-pc");
 const pcLogo = document.getElementById("js-nav-pc-logo");
 
 function changeHeaderSize() {
   const currentY = window.scrollY;
   if (currentY > 0) {
     main.classList.add("is-nav-small");
-    spHeader.classList.add("is-nav-small");
-    pcHeader.classList.add("is-nav-small");
+    navSP.classList.add("is-nav-small");
+    navPC.classList.add("is-nav-small");
     pcLogo.src = "public/assets/images/logo-small.png";
   } else {
     main.classList.remove("is-nav-small");
-    spHeader.classList.remove("is-nav-small");
-    pcHeader.classList.remove("is-nav-small");
+    navSP.classList.remove("is-nav-small");
+    navPC.classList.remove("is-nav-small");
     pcLogo.src = "public/assets/images/logo.png";
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  spHeader.style.transition = "none";
-  pcHeader.style.transition = "none";
+  navSP.style.transition = "none";
+  navPC.style.transition = "none";
   main.style.transition = "none";
   changeHeaderSize();
-  spHeader.style.transition = "";
-  pcHeader.style.transition = "";
+  navSP.style.transition = "";
+  navPC.style.transition = "";
   main.style.transition = "";
 });
 
@@ -36,7 +36,8 @@ window.addEventListener("scroll", () => {
 });
 
 //hamburger
-const headerOpen = document.getElementById("js-nav-pc");
+const hamburgerNav = document.getElementById("js-nav-pc");
+const hamburgerNavBg = document.getElementById("js-nav-sp-background");
 const btnClose = document.getElementById("js-nav-sp-btn-close");
 const btnOpen = document.getElementById("js-nav-sp-btn-open");
 const stopScroll = (event) => {
@@ -44,14 +45,17 @@ const stopScroll = (event) => {
 };
 
 function openNav() {
-  headerOpen.classList.add("is-nav-open");
+  hamburgerNav.classList.add("is-nav-open");
+  hamburgerNavBg.classList.add("is-nav-open");
   document.addEventListener("touchmove", stopScroll, { passive: false });
   document.addEventListener("mousewheel", stopScroll, { passive: false });
 }
 function closeNav() {
-  headerOpen.classList.remove("is-nav-open");
+  hamburgerNav.classList.remove("is-nav-open");
+  hamburgerNavBg.classList.remove("is-nav-open");
   document.removeEventListener("touchmove", stopScroll);
   document.removeEventListener("mousewheel", stopScroll);
 }
 btnOpen.addEventListener("click", openNav);
 btnClose.addEventListener("click", closeNav);
+hamburgerNavBg.addEventListener("click", closeNav);
